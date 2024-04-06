c62c7f2f01b78fa8cdaa08dd6b65b672949009bf
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-06 10:22:07 +0530

    Initial commit.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center mt-10 text-center poppins-regular">
+      <h1 className="text-2xl font-semibold tracking-widest opacity-80 poppins-medium">
+        FILE{"  "}SHARE
+      </h1>
+    </main>
+  );
+}
+
+export default App;
